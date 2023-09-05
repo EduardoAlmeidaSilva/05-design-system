799cb8fcc56acc6542638267781a984ad4458fcd
Eduardo Almeida <eduardo.almeida1106@gmail.com>
2023-09-05 15:43:29 -0300

    fix: Add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -13,10 +13,10 @@ const config = {
   stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-essentials"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
